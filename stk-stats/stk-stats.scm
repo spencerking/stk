9c51d9d8c572ff199c7f2f93579e4c53d66f2cdc
Spencer King <spencerking@users.noreply.github.com>
2019-12-26 20:45:59 -0600

    stk-stats > mean

---
+++ stk-stats/stk-stats.scm
@@ -0,0 +1,10 @@
+(define (internal-sum-list x)
+  (if (null? x) 0
+      (+ (car x) (internal-sum-list (cdr x)))))
+
+(define (internal-count-list x)
+  (if (null? x) 0
+      (+ 1 (internal-count-list (cdr x)))))
+
+(define (mean l)
+  (/ (internal-sum-list l) (internal-count-list l)))
---
+++ stk-stats/tests.scm
@@ -0,0 +1,4 @@
+(load "stk-stats.scm")
+
+(define test-list (list 1 2 3 4 5))
+(display (mean test-list))
